a0d6020c7526175a6d5f2ef1d60ac6fa239850d7
JankowiakAdam <jankowiakadam0902@gmail.com>
2022-02-23 16:02:32 +0100

    CwiczeniaNr1
    Zadania 1-7

---
+++ CwiczeniaNr1/Zadanie4.py
@@ -0,0 +1,4 @@
+x = input("Wprowadz liczbę pierwszą: ")
+y = input("Wprowadz liczbę drugą: ")
+print(y)
+print(x)
---
+++ CwiczeniaNr1/Zadanie5.py
@@ -0,0 +1,4 @@
+x = int(input("Wprowadz liczbę pierwszą: "))
+y = int(input("Wprowadz liczbę drugą: "))
+z = int(input("Wprowadz liczbę trzecią: "))
+print(x*y*z)
